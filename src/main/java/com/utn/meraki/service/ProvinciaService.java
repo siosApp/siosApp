02f383c8027d3b6971b5fd7fa5372231d33c842b
Seba Ruiz <seba.lruiz@gmail.com>
2018-08-09 17:20:44 -0300

    commit seba

--- src/main/java/com/utn/meraki/service/ProvinciaService.java
+++ src/main/java/com/utn/meraki/service/ProvinciaService.java
@@ -3,7 +3,7 @@ package com.utn.meraki.service;
 import java.util.List;
 
 import com.utn.meraki.model.ProvinciaModel;
-
+//Provincia
 public interface ProvinciaService {
 	
 	public abstract ProvinciaModel crearProvincia(ProvinciaModel provinciaModel);
