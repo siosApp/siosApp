f06206bae0bc49f66d87ddb563a4d9f856ba661a
Tincho Castronovo <tinchocastronovo@gmail.com>
2018-09-04 15:08:59 -0300

    Modelo UsuarioDestacado

---
+++ src/main/java/com/utn/meraki/model/UsuarioDestacado.java
@@ -0,0 +1,17 @@
+package com.utn.meraki.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsuarioDestacado {
+	
+	//ATRIBUTOS
+	private String idUsuario;
+	
+	private String nombre;
+	
+	private String apellido;
+	
+	private List<RubroModel> rubros = new ArrayList<>();
+
+}
