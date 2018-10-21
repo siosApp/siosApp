45edf2819ccd33078b6fe4e39adf554457f093ec
eb-dante <dante@eventbrite.com>
2018-10-21 09:33:14 -0300

    nuevas pruebas añadidas

--- src/test/java/pruebasUnitarias/InicioSesionContraseñaIncorrecta.java
+++ src/test/java/pruebasUnitarias/InicioSesionContraseñaIncorrecta.java
@@ -1,34 +1,36 @@
 package pruebasUnitarias;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class InicioSesionContraseñaIncorrecta {
+import configuracionPruebas.ConfiguracionGeneralPruebas;
+
+public class InicioSesionContraseñaIncorrecta extends ConfiguracionGeneralPruebas {
 
 	@Test
-	public void test() {
-		String projectLocation = System.getProperty("user.dir");
+	public void intentarIngresarConContraseñaIncorrecta() {
 		
-		System.setProperty("webdriver.chrome.driver", projectLocation+"/chromeDriver/chromedriver");
-
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = super.configurarSeleniumWebDriver();
 		driver.get("http://localhost:4200/login");
-		driver.manage().window().maximize();
+		driver.manage().window().setSize(new Dimension(1440, 900));
 		
-		String usuario = "/html/body/app-root/app-login/div[3]/div[1]/div[2]/form/div[1]/div/input";
-		String contraseña = "/html/body/app-root/app-login/div[3]/div[1]/div[2]/form/div[2]/div/input";
-		String botonIngresarAlSistema = "/html/body/app-root/app-login/div[3]/div[1]/div[2]/form/div[4]/div/button";
-		
-		driver.findElement(By.xpath(usuario)).sendKeys("eduardo");
+		// Ingresa un usuario correcto
+		driver.findElement(By.xpath(super.getUsuarioXpath())).sendKeys("eduardo");
 		
 		// Ingresa una contraseña incorrecta
-		driver.findElement(By.xpath(contraseña)).sendKeys("1234567");
+		driver.findElement(By.xpath(super.getContraseñaXpath())).sendKeys("12345");
 		
 		// Intenta ingresar con una contraseña incorrecta
-		driver.findElement(By.xpath(botonIngresarAlSistema)).click();	
+		driver.findElement(By.xpath(super.getBotonIngresarAlSistemaXpath())).click();
 		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		//driver.quit();
+	
 	}
 
 }
