90a4c7b02af20110a884619d1117a4073c4c9f4f
Tincho Castronovo <tinchocastronovo@gmail.com>
2018-10-25 20:16:39 -0300

    Se arregló pequeño bug (corchete faltante) del servicio de destacados

--- src/main/java/com/utn/meraki/service/impl/DestacadoServiceImpl.java
+++ src/main/java/com/utn/meraki/service/impl/DestacadoServiceImpl.java
@@ -42,6 +42,7 @@ public class DestacadoServiceImpl implements DestacadoService{
 					}else {
 						UsuarioDestacadoModel usuarioDestacadoModel = usuarioDestacadoConverter.convertDestacadoToUsuarioDestacadoModel(destacado);
 						usuariosDestacados.add(usuarioDestacadoModel);
+					}
 				}
 			}
 		}
